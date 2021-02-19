20f882c261570ddcea55d7efe6252c0b008990cc
mustafahmadov <mustafaia@code.edu.az>
2021-02-20 02:19:20 +0400

    carousel section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,19 @@ $(document).ready(function(){
         $('.bottom').removeClass("clicked");
       });
 })
+
+let stickyHeader = document.querySelector("header");
+    let sticky = header.offsetTop;
+    console.log(header.offsetTop);
+    console.log(window.pageYOffset);
+    function makeStickyNav() {
+        if (window.pageYOffset > sticky) {
+            stickyHeader.classList.add("sticky-top");
+
+        } else {
+            stickyHeader.classList.remove("sticky-top");
+        }
+    }
+    $(window).scroll(function () {
+        makeStickyNav();
+    });
